8f282cfc29b5b3410761393683fc72359c4931c9
parasnoor <noorp6753@gmail.com>
2024-10-12 09:42:05 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+// chapter#01
+alert("Hello, world!");
+alert("Welcome to my website!");
+Alert("Welcome to my website!"); // This will cause an error
+alert("Hello, world!");
+alert("Welcome to pro coder"); //alert built-in JavaScript function that creates a pop-up dialog box,arentheses are used to enclose the arguments passed to the function,double quotes " show it is string,, semicolon marks the end of the statement. 
+
+// chapter#02
+var message = "Hello, world!";
+alert(message);
+// Q#02
+var age;
+age = 25;
+alert(age);
+// Q#03
+// Declare a variable named greeting and assign it the value "Good morning!"
+var greeting = "Good morning!";
+alert(greeting);
+// Change the value of greeting to "Good evening!"
+greeting = "Good evening!";
+alert(greeting);
+// Q#04
+var favoriteColor = "blue";
+// Display an alert that includes the variable
+alert("My favorite color is " + favoriteColor);
+// Q#05
+var age;
+age = 25;
+alert(age);
+
+
+
+
